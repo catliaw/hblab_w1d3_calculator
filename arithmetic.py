71b4a61b6c3115f90fd36f7674bebfa1aed8509e
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 13:01:05 -0700

    Fixed Tab/Space issue, corrected grammar.

--- arithmetic.py
+++ arithmetic.py
@@ -1,18 +1,18 @@
 def add(num1, num2):
 	"""Takes 2 integers and returns the sum of the inputs."""
-    return num1 + num2
+	return num1 + num2
 
 def subtract(num1, num2):
 	"""Takes 2 integers and returns the result of subtracting the second number from the first."""
-    return num1 - num2
+	return num1 - num2
 
 def multiply(num1, num2):
-   	"""Takes 2 integers and returns the product of the inputs."""
-   	return num1 * num2
+	"""Takes 2 integers and returns the product of the inputs."""
+	return num1 * num2
 
 def divide(num1, num2):
-    """Takes 2 integers, divides the first input by the second, returning a floating point number."""
-    return float(num1) / num2
+	"""Takes 2 integers, divides the first input by the second, returning a floating point number."""
+	return float(num1) / num2
 
 def square(num1):
     """Takes 1 integer and returns the square of that integer."""
@@ -23,7 +23,7 @@ def cube(num1):
     return num1 ** 3
 
 def power(num1, num2):
-   	"""Takes 2 integers and returns the result of raising the first integer to the power of the second integers."""
+   	"""Takes 2 integers and returns the result of raising the first integer to the power of the second integer."""
    	return num1 ** num2
 
 def mod(num1, num2):
